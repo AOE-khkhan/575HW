e2d01d04f901929c935e0c23307f78f5a5f253c0
Landon <landon.wright91@gmail.com>
2018-03-13 00:03:45 -0600

    Something is happening

--- geneticOpt/Optimizer.py
+++ geneticOpt/Optimizer.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 
 class Optimizer:
-    def __init__(self, x, fitness, n_generations=5, population_size=30, constraint=None):
+    def __init__(self, x, fitness, n_generations=5, population_size=5, constraint=None):
         """Initialize the optimizer
-        x: list that contains a series of dictionaries that define the x values of the form {type: (min, max)} where
-        type is either continuous or integer. and min and max are the minimum and maximum values of the variable
+        x: list that contains a series of dictionaries that define the x values for the optimizer where
+        type is either continuous or integer. and min and max are the minimum and maximum values of the variable this
+        should follow the form [{'type': ('integer' or 'continuous'), 'bounds': (min, max)]] with a new dict for each
+        x value
 
         fitness: function that accepts x array in the same order as given in the x input, and returns the fitness of
         each design.
@@ -19,24 +21,28 @@ class Optimizer:
         suggests that the constraint is satisfied.
         """
         self.num_generations = n_generations
-        self.num_population = population_size
+        self.num_population = np.trunc(population_size)
+        if self.num_population % 2 != 0:
+            self.num_population += 1
+        self.num_population = int(self.num_population)
         self.num_x = len(x)
         self.fitness_func = fitness
-        self.variable_types = np.zeros(self.num_x)
+        self.x_def = x
+        # self.variable_types = np.zeros(self.num_x)
 
         self.tournament_size = 2
         self.crossover_prob = 0.7
-        self.mutation_prob = 0.1
+        self.mutation_prob = 0.7
         self.cross_eta = 0.5
         self.mutation_beta = 0.2
 
-        for i, var in enumerate(x):
-            if list(var.keys())[0].lower() == 'integer':
-                self.variable_types[i] = 0
-            elif list(var.keys())[0].lower() == 'continuous':
-                self.variable_types[i] = 1
-            else:
-                print("error unknown variable type")
+        # for i, var in enumerate(x):
+        #     if list(var.keys())[0].lower() == 'integer':
+        #         self.variable_types[i] = 0
+        #     elif list(var.keys())[0].lower() == 'continuous':
+        #         self.variable_types[i] = 1
+        #     else:
+        #         print("error unknown variable type")
 
         # initialize the parent array
         # stored in the form ([fitness, x1, x2, x3, ..., xn])
@@ -44,17 +50,15 @@ class Optimizer:
         for i in range(self.num_population):
             x_new = np.zeros(self.num_x)
             for j, val in enumerate(x):
-                key = list(val.keys())[0]
-                if key.lower() == 'integer':
-                    x_new[j] = np.random.randint(val[key][0], val[key][1]+1)
-                elif key.lower() == 'continuous':
-                    x_new[j] = np.random.uniform(val[key][0], val[key][1]+0.0000001)
+                if val['type'] == 'integer':
+                    x_new[j] = np.random.randint(val['bounds'][0], val['bounds'][1]+1)
+                elif val['type'] == 'continuous':
+                    x_new[j] = np.random.uniform(val['bounds'][0], val['bounds'][1])
                 else:
                     print("error unknown variable type")
             self.population[i, 0] = self.fitness_func(x_new)
             self.population[i, 1:] = x_new
         self.population = sort_array_by_col(self.population, 0)
-        test = randomize_array_by_col(self.population)
         return
 
 
@@ -69,7 +73,7 @@ class Optimizer:
             rand_indicies = np.random.randint(parents.shape[0], size=self.tournament_size)
             competitors = self.population[rand_indicies]
             sorted_competitors = sort_array_by_col(competitors, 0)
-            parents[row, :] = sorted_competitors[0, :]
+            parents[row, :] = sorted_competitors[-1]
         return parents
 
 
@@ -93,27 +97,47 @@ class Optimizer:
                             a = ((2 * r) ** (1/self.cross_eta)) / 2
                         else:
                             a = 1 - ((2 - 2 * r)**(1/self.cross_eta)) / 2
-                        child1[x_idx] = a * child1[x_idx] + (1-a) * child2[x_idx]
-                        child2[x_idx] = (1-a) * child1[x_idx] + a * child2[x_idx]
+                        child1_val = child1[x_idx]
+                        child2_val = child2[x_idx]
+                        child1[x_idx] = a * child1_val + (1-a) * child2_val
+                        child2[x_idx] = (1-a) * child1_val + a * child2_val
                         # truncate the values if needed
-                        if self.variable_types[x_idx] == 0:
-                            child1[x_idx] = np.trunc(child1[x_idx])
-                            child2[x_idx] = np.trunc(child2[x_idx])
+                        if self.x_def[x_idx]['type'] == 'integer':
+                            child1[x_idx] = np.round(child1[x_idx])
+                            child2[x_idx] = np.round(child2[x_idx])
 
                     if mutate1 < self.mutation_prob:
+                        child1 = self.mutate(child1, x_idx, self.x_def[x_idx]['bounds'],
+                                             self.x_def[x_idx]['type'], generation)
 
-                        # r = np.random.uniform(min, max)
-                        alpha = (1 - (generation - 1) / self.num_generations) ** self.mutation_beta
-
-
-
-
-
-            # find the fitness of parents and children
-
+                    if mutate2 < self.mutation_prob:
+                        child2 = self.mutate(child2, x_idx, self.x_def[x_idx]['bounds'],
+                                             self.x_def[x_idx]['type'], generation)
+                # put the children into the children array
+                child1_fitness = self.fitness_func(child1)
+                child2_fitness = self.fitness_func(child2)
+                children[idx, 0] = child1_fitness
+                children[idx, 1:] = child1
+                children[idx+1, 0] = child2_fitness
+                children[idx+1, 1:] = child2
             # perform elitism
-            pass
-        pass
+            population_pool = np.append(parents, children, axis=0)
+            population_pool = sort_array_by_col(population_pool, 0)
+            self.population = population_pool[0:self.num_population]
+            print(generation)
+
+    def mutate(self, child, idx, bounds, type, generation):
+        min = bounds[0]
+        max = bounds[1]
+        r = np.random.uniform(min, max)
+        alpha = (1 - (generation - 1) / self.num_generations) ** self.mutation_beta
+        if r <= child[idx]:
+            child[idx] = min + (r - min) ** alpha * (child[idx] - min) ** (1 - alpha)
+        else:
+            child[idx] = max - (max - r) ** alpha * (max - child[idx]) ** (1 - alpha)
+        if type == 'integer':
+            child[idx] = np.round(child[idx])
+        return child
 
 
 def fitt(x):
@@ -126,11 +150,9 @@ def sort_array_by_col(array, sort_col=0):
     new_array = array[np.argsort(array[:, sort_col])]
     return new_array
 
-def randomize_array_by_col(array):
-    new_array = array[np.random.shuffle(np.arange(array.shape[0]))]
-    return new_array
 
 if __name__ == "__main__":
-    z = [{'integer': (2, 4)}, {'continuous': (1, 10)}]
+    z = [{'type': 'integer', 'bounds': (2, 7)}, {'type': 'continuous', 'bounds': (1, 11)}]
 
-    opt = Optimizer(z, fitt)
+    opt = Optimizer(z, fitt, n_generations=100, population_size=20)
+    opt.find_min()
